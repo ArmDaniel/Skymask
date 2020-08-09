fedd0c4cc7c56b119d085fe18ebd05d65b02b59d
Araniel <44145743+Motanovici@users.noreply.github.com>
2020-08-09 18:06:33 +0300

    Update skyUI.py

--- skyUI.py
+++ skyUI.py
@@ -1,14 +1,50 @@
 from tkinter import *
 from tkinter import filedialog
+from tkinter import messagebox
+from prototype import *
+
+
 root = Tk()
+
+#######################################
+#                                     #
+#             TEMPMAIL                #  
+#                                     #
+#######################################
+
+# get user input when calling func
+
+
+def mail_check():
+	messagebox.showinfo(title = "Current Inbox", message=check_mail("name","@1secmail.org"))
+
+inbox = Button(root,text="Check Inbox", command=mail_check)
+inbox.pack()
+
+def msg_fetch():
+        messagebox.showinfo(title= "Message", message=fetch_msg("example","domain","mid"))
+
+crtMsg = Button(root,text="Retrieve specific message",command=msg_fetch())
+crtMsg.pack()
+
+
+
+########################################
+#                                      #
+#          Skynet Specifics            #
+#                                      #
+########################################
 
 def browsefunc():
     filename = filedialog.askopenfilename()
     pathlabel.config(text=filename)
+    messagebox.showinfo(title="Skylink", message = "Your Skylink: " + upload_f(filename))
+     
 
-browsebutton = Button(root, text="Browse", command=browsefunc)
+browsebutton = Button(root, text="Upload", command=browsefunc)
 browsebutton.pack()
 
-#prints label
+#prototype
 pathlabel = Label(root)
-pathlabel.pack()
+
+
